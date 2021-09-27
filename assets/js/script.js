7407ca65c07a1312789f85590dba8a9c7b1625f8
dan13l80 <88808310+dan13l80@users.noreply.github.com>
2021-09-26 22:08:01 -0400

    Save events added when browser refreshed

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,10 +37,25 @@ timeUpdate();
 
 
 //when save is selected - Text saved to localstorage
-
 $(".saveBtn").on("click", function () {
     var descr = $(this).siblings(".description").val();
     var hour = $(this).siblings(".hour").text();
 
     localStorage.setItem(hour, descr);
+})
+
+//When Page refreshed - Save events persist
+$(".time-block").each(function () {
+    var savedhourNode = $(this).children(".hour");
+    var savedHour = savedhourNode[0].innerHTML;
+    console.log('savedHour: ', savedHour);
+    var savedVal = localStorage.getItem(savedHour);
+    
+    console.log(savedVal)
+
+    if (savedVal !== null) {
+        console.log('IT RAN')
+        $(this).children(".description").text(savedVal);
+    }
+
 })
